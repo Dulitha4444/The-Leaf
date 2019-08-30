aa984648b8727720580325efc16fa871597758cf
Dulitha4444 <53008839+Dulitha4444@users.noreply.github.com>
2019-08-30 15:05:45 +0530

    Add

---
+++ Customer 1/Customer_Registration2.php
@@ -0,0 +1,39 @@
+<?php
+
+require('mb.php');
+
+if (isset($_POST['username']) and isset($_POST['lastname'])){
+
+$Username = $_POST['username'];
+$Lastname = $_POST['lastname'];
+$Age = $_POST['age'];
+$Nation = $_POST['nation'];
+$Veg = $_POST['veg'];
+$Ptype = $_POST['ptype'];
+$Ftimes = $_POST['ftimes'];
+$Address = $_POST['address'];
+$Email = $_POST['email'];
+$Password= $_POST['password'];
+$date = date("Y-m-d H:i:s");
+$user_type = 'customer';
+
+
+
+
+$sql = "INSERT INTO customer_registration (user_name, last_name, age, nation, veg, ptype, ftimes, address, email, password, recive_date)
+VALUES ('$Username', '$Lastname', '$Age', '$Nation', '$Veg', '$Ptype', '$Ftimes', '$Address', '$Email', '$Password', '$date')";
+ $sql1 = "INSERT INTO login_details (user_name, user_password, user_type)
+ VALUES ('$Username', '$Password', '$user_type')";
+$connection->query($sql1);
+
+if ($connection->query($sql) === TRUE) {
+    echo "Successfully saved.";
+
+} else {
+    echo "Save Failed: " . $sql . "<br>" . $connection->error;
+}
+
+$connection->close();
+}
+
+?>
